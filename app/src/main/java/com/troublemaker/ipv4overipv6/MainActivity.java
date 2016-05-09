da1a1143102f4e922c29ebbbfd97259857796861
yanhao <yanhao691025518@gmail.com>
2016-05-09 20:14:05 +0800

    yanhao update

--- app/src/main/java/com/troublemaker/ipv4overipv6/MainActivity.java
+++ app/src/main/java/com/troublemaker/ipv4overipv6/MainActivity.java
@@ -1,17 +1,31 @@
 package com.troublemaker.ipv4overipv6;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.VpnService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.IntentFilter;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements Serializable {
     final static String TAG = "MainActivity";
-
+    private MsgReceiver msgReceiver;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        msgReceiver = new MsgReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.troublemaker.ipv4overipv6.RECEIVER");
+        registerReceiver(msgReceiver, intentFilter);
         Intent intent = VpnService.prepare(this);
         if (intent != null) {
             Log.d(TAG, "Ask user to confirm VPN connection");
@@ -25,8 +39,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int request, int result, Intent data) {
         if (result == RESULT_OK) {
-            Intent intent = new Intent(this, MyVpnService.class);
+            intent = new Intent(this, MyVpnService.class);
+           // Bundle bundle = new Bundle();
+            //bundle.putSerializable("main", this);
+            //intent.putExtras(bundle);
             startService(intent);
         }
+    }
+    @Override
+    protected void onDestroy() {
+        //停止服务
+        stopService(intent);
+        //注销广播
+        unregisterReceiver(msgReceiver);
+        super.onDestroy();
+    }
+    public void change(String s){
+        TextView tv = (TextView) findViewById(R.id.showText);
+        tv.setText(s);
+    }
+//    public void showTraffic(byte[] data) throws Exception {
+//        //handle the traffic
+//        TextView tv = (TextView) findViewById(R.id.showText);
+//        tv.setText(data.toString());
+//
+//    }
+    public class MsgReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent rintent) {
+            //拿到进度，更新UI
+            String data = rintent.getStringExtra("data");
+            TextView tv = (TextView) findViewById(R.id.showText);
+            tv.setText(data.toString());
+        }
+
     }
 }
--- app/src/main/java/com/troublemaker/ipv4overipv6/MyVpnService.java
+++ app/src/main/java/com/troublemaker/ipv4overipv6/MyVpnService.java
@@ -5,9 +5,12 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.lang.String;
+import  java.lang.Object;
 
 import android.app.AlertDialog.Builder;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.Handler;
@@ -15,33 +18,35 @@ import android.os.Environment;
 import android.os.Message;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
+import android.app.Activity;
 
 public class MyVpnService extends VpnService implements Handler.Callback,Runnable {
     private static final String TAG = "MyVpnService";
     private Handler mHandler;
     private Thread mThread;
+    private Thread trafficThread;
     private String mServerAddress;
     private String mServerPort;
     private PendingIntent mConfigureIntent;
     private ParcelFileDescriptor myinterface;
     private String myParameters;
-    private byte[] readIPBuf;
     private byte[] readTrafficBuf;
     private boolean ipFlag;
 
     private File extDir;
-    private File ipFile;
-    private FileOutputStream ipFileOutputStream;
-    private BufferedOutputStream ipOut;
-    private FileInputStream ipFileInputStream;
-    private BufferedInputStream ipIn;
 
     private File trafficFile;
     private FileOutputStream trafficFileOutputStream;
     private  BufferedOutputStream trafficOut;
     private  FileInputStream trafficFileInputStream;
     private BufferedInputStream trafficIn;
+    private  MainActivity mActivity;
+    private TextView showText;
+
+    private Intent mIntent = new Intent("com.troublemaker.ipv4overipv6.RECEIVER");
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -53,7 +58,8 @@ public class MyVpnService extends VpnService implements Handler.Callback,Runnabl
         }
         ipFlag = false;
         mThread = new Thread(this, "MyVpnThread");
-        Log.d(TAG,"thread start");
+        Log.d(TAG, "thread start");
+
         mThread.start();
         return START_STICKY;
     }
@@ -91,19 +97,19 @@ public class MyVpnService extends VpnService implements Handler.Callback,Runnabl
 
     @Override
     public synchronized void run() {
+        //TextView tv = (TextView) mActivity.findViewById(R.id.showText);
+        //tv.setText("i changed");
         String ip_info = initBackend();
         Log.d(TAG,ip_info);
-
-
         try {
-            extDir = Environment.getExternalStorageDirectory();//获取当前路径
-            //Log.d(TAG,extDir.toString());
+            extDir = this.getFilesDir();//获取当前路径
+            Log.d(TAG,extDir.toString());
 
-//            trafficFile = new File(extDir,"traffic_pipe");
-//            trafficFileOutputStream = new FileOutputStream(trafficFile);
-//            trafficOut = new BufferedOutputStream(trafficFileOutputStream);
-//            trafficFileInputStream = new FileInputStream(trafficFile);
-//            trafficIn = new BufferedInputStream(trafficFileInputStream);
+            trafficFile = new File(extDir,"TRAFFIC_INFO_PIPE");
+            trafficFileOutputStream = new FileOutputStream(trafficFile);
+            trafficOut = new BufferedOutputStream(trafficFileOutputStream);
+            trafficFileInputStream = new FileInputStream(trafficFile);
+            trafficIn = new BufferedInputStream(trafficFileInputStream);
 
             while (true){
                 if (!ipFlag){
@@ -113,12 +119,13 @@ public class MyVpnService extends VpnService implements Handler.Callback,Runnabl
                     }
                 }else{
                     Log.d(TAG,"XXXXXXXXXXXXXXXXXXXXXXXXX");
-//                    int readTrafficLen = trafficIn.read(readTrafficBuf);
-//                    if (readTrafficLen>0){
-//                        showTraffic(readTrafficBuf);
-//                        Toast.makeText(getApplicationContext(), readTrafficBuf.toString(),
-//                                Toast.LENGTH_SHORT).show();
-//                    }
+                    int readTrafficLen = trafficIn.read(readTrafficBuf);
+                    Log.d(TAG,readTrafficLen+" sdfasdfasdf");
+                    if (readTrafficLen>0){
+                        showTraffic(readTrafficBuf);
+                        Toast.makeText(getApplicationContext(), readTrafficBuf.toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
                 }
                 Thread.sleep(2000);
             }
@@ -159,11 +166,18 @@ public class MyVpnService extends VpnService implements Handler.Callback,Runnabl
                 .setConfigureIntent(mConfigureIntent)
                 .establish();
         myParameters = parameters;
-        int k = myinterface.getFd();
-        Log.d(TAG,k+"");
-        startBackend(k);
+        final int k = myinterface.getFd();
+        Log.d(TAG, k + "");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startBackend(k);
+            }
+        }).start();
     }
     private void showTraffic(byte[] data) throws Exception {
         //handle the traffic
+        mIntent.putExtra("data", data.toString());
+        sendBroadcast(mIntent);
     }
 }
